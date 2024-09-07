7617a1ebb33c8e3f1364ce8a0630873dd111969b
Jhonel <jgapazap@unjbg.edu.pe>
2024-09-07 18:14:04 -0500

    Factorial recursiva

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+	
+	if(n==0){
+		return 1;
+	} else {
+		return n*factorial(n-1);
+	}
+}
+
+int main(){
+	
+	int numero;
+	cout << "Ingrese un numero: ";
+	cin >> numero;
+	cout << endl << "La factorial de " << numero << " es: " << factorial(numero) << endl;
+	return 0;
+}
